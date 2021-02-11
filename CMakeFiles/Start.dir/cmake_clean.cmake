19cabeac7fd144f9c8e5b41e94f71a4cac62493a
Ivan <ivan.ivkovichh@gmail.com>
2021-02-11 22:11:39 +0100

    Wee

---
+++ CMakeFiles/Start.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Start.dir/main.cpp.o"
+  "Start"
+  "Start.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
